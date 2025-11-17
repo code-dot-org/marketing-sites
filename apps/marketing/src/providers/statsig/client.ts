5aacf0fdc099f05d0838922ba1fa992d1a997663
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2025-11-17 10:04:04 -0800

    fix(statsig): update stable ID handling for Code.org brand
    Refactor getStatsigStableId to conditionally return stable ID based on brand.

--- apps/marketing/src/providers/statsig/client.ts
+++ apps/marketing/src/providers/statsig/client.ts
@@ -12,15 +12,16 @@ import OneTrustContext, {
 } from '@/providers/onetrust/context/OneTrustContext';
 import plugins from '@/providers/statsig/plugins';
 
-function getStatsigStableId() {
+function getStatsigStableId(brand: Brand) {
   const onetrustContext = useContext(OneTrustContext);
+  let stableId = getCookie('statsig_stable_id');
 
   if (!onetrustContext?.allowedCookies.has(OneTrustCookieGroup.Performance)) {
     // If the user has not allowed performance cookies, we do not set a stable ID
-    return undefined;
+    // For the Code.org brand, the stable ID is retrieved from cookie to track users across subdomains
+    // For the other brand, we do not set a stable ID as we do not need cross-domain tracking
+    return brand === Brand.CODE_DOT_ORG ? stableId : undefined;
   }
-
-  let stableId = getCookie('statsig_stable_id');
 
   if (!stableId) {
     stableId = uuidv4();
@@ -39,7 +40,7 @@ export function getClient(clientKey: string, stage: Stage, brand: Brand) {
   // Add stableID only for code.org brand so we can track users across
   // studio.code.org and code.org, otherwise fallback to Statsig SDK's default behavior
   const stableId =
-    brand === Brand.CODE_DOT_ORG ? getStatsigStableId() : undefined;
+    brand === Brand.CODE_DOT_ORG ? getStatsigStableId(brand) : undefined;
   const user: StatsigUser = stableId ? {customIDs: {stableID: stableId}} : {};
   return useClientAsyncInit(clientKey, user, {
     environment: {tier: stage},
